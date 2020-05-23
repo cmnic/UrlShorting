aac69f45015b0be01cdd5437ddf16924885d0083
立言 <58519324+liyanqwq@users.noreply.github.com>
2020-05-23 13:38:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (date("i")%20 == 0) {
 //开始判断处理
 if ($status == "undefind" || empty($status)) {
     echo("<br / ><center><br / ><img src=\"https://3gimg.qq.com/tele_safe/safeurl/img/notice.png\" widht=\"85\"  height=\"85\" alt=\"错误\"></center>");
-    echo('<center><h2>你访问的页面不存在!</h2></center>');
+    echo('<center><h2>404 Not Found</h2></center>');
     require_once "footer.php";
     exit();
 }
@@ -28,23 +28,14 @@ if ($status == "passmessage") {
       <div class=\"mdui-card.mdui-card-media-covered-transparent\">
         <div class=\"mdui-card-primary\">
           <div class=\"mdui-card-primary-subtitle\">$timemessage</div>
-            <center>
-              <div class=\"mdui-card-primary-title\" style=\"word-break:break-all;\">
-                「" . htmlspecialchars($information) . "」
-              </div>
+            <center class=\"mdui-typo\">
+              <pre style=\"user-select:all;max-width:900px;\">" . htmlspecialchars($information) . "</pre>
+              <h5>你收到了一条悄悄话！ <a class=\"mdui-text-color-grey-400\" href=\"$url\">我也要写</a></h5>
             </center>
           </div>
         </div>
       </div>
     <br/>
-    <div class=\"mdui-card.mdui-card-media-covered-transparent\">
-    <br />
-    <h4>&emsp;&emsp;Q:这是什么?</h4>
-    <h5>&emsp;&emsp;A:这是别人发给你的一条密语!</h5><br/>
-    <h4>&emsp;&emsp;Q:我也想写密语怎么办?</h4>
-    <h5>&emsp;&emsp;A:访问<a class=\"mdui-text-color-grey-800\" href=\"$url\">$url</a>平台你可以免费进行密语缩短</h5>
-    <br />
-    </div>
       ";
       require_once "footer.php";
       exit();
@@ -55,22 +46,29 @@ if ($status == "passmessage") {
 <br/>
 <div class="mdui-container doc-container">
     <div class="mdui-typo">
-        <h2>短域</h2>
-        <div class="mdui-textfield">
-            <input id="content" time="content" class="mdui-textfield-input" type="text" placeholder="请输入链接或密语" />
+    	<h2>悄悄话</h2>
+        <div class="mdui-textfield mdui-textfield-floating-label">
+        	<i class="mdui-icon material-icons">&#xe0b9;</i>
+        	<label class="mdui-textfield-label">请输入悄悄话</label>
+            <textarea id="wordcontent" time="content" class="mdui-textfield-input" type="text"></textarea>
         </div>
-        <button onClick="Submit();" id="Submit" class="mdui-btn mdui-btn-dense mdui-color-theme-accent mdui-ripple">
-          <i class="mdui-icon material-icons">send</i>
+        <br />
+        <button onClick="wordSubmit();" id="wordSubmit" class="mdui-btn mdui-btn-dense mdui-color-indigo mdui-ripple">
+          <i class="mdui-icon material-icons">&#xe163;</i> 发送
         </button>
-        <label class="mdui-radio">
-          <input type="radio" name="type" id="type"  value="shorturl" checked />
-          <i class="mdui-radio-icon"></i>短域
-        </label>
-        &emsp;&emsp;
-        <label class="mdui-radio">
-          <input type="radio" name="type" id="type"  value="passmessage" />
-          <i class="mdui-radio-icon"></i>密语
-        </label>
+        <br />
+        <br />
+        <hr/>
+        <h2>网址缩短</h2>
+        <div class="mdui-textfield mdui-textfield-floating-label">
+        	<i class="mdui-icon material-icons">&#xe226;</i>
+        	<label class="mdui-textfield-label">请输入链接</label>
+            <input id="content" time="content" class="mdui-textfield-input" type="url" />
+            <div class="mdui-textfield-error">请输入正确的链接</div>
+        </div>
+        <button onClick="Submit();" id="Submit" class="mdui-btn mdui-btn-dense mdui-color-indigo mdui-ripple">
+          <i class="mdui-icon material-icons">&#xe163;</i> 缩短
+        </button>
     </div>
 </div>
 <script>
@@ -84,22 +82,37 @@ if ($status == "passmessage") {
       }     
   }
   function Submit() {
-    document.getElementById("Submit").innerHTML = "处理中...";
+    document.getElementById("progressbar").setAttribute("style","display:block;");
     var content = document.getElementById("content").value;
-    var type = getRadioBoxValue("type");
-    if(type == "shorturl")
-    {
-        var content = escape(content);
-    }
+    var type = 'shorturl';
+    var content = escape(content);
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "./submit.php");
     xhr.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
     xhr.send("content=" + content + "&type=" + type);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById("Submit").innerHTML = "<i class=\"mdui-icon material-icons\">send</i>";
+            document.getElementById("progressbar").setAttribute("style","display:none;");
             if (xhr.responseText == 200) {
-                mdui.snackbar("缩短成功!");
+                window.location.href="shorturl.php";
+            } else {
+                mdui.snackbar(xhr.responseText);
+            }
+        }
+    }
+  }
+  function wordSubmit() {
+    document.getElementById("progressbar").setAttribute("style","display:block;");
+    var content = document.getElementById("wordcontent").value;
+    var type = 'passmessage';
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "./submit.php");
+    xhr.setRequestHeader('Content-Type', ' application/x-www-form-urlencoded');
+    xhr.send("content=" + content + "&type=" + type);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("progressbar").setAttribute("style","display:none;");
+            if (xhr.responseText == 200) {
                 window.location.href="shorturl.php";
             } else {
                 mdui.snackbar(xhr.responseText);
@@ -110,12 +123,13 @@ if ($status == "passmessage") {
 </script>
 <div class="mdui-container doc-container">
     <div class="mdui-typo">
-         <h2>帮助</h2>
-         1.输入短域请加上http(s)://<br />
-         2.中文域名请手动Punycode编码后再使用<br />
-         3.网址最长支持1000字符<br />
-         4.密语最长支持3000字符(合1000汉字)<br />
-         5.其余详见菜单-帮助界面
+         <h2>使用须知</h2>
+         <ol>
+         	<li>使用时请遵守您所在国家/地区法律。</li>
+         	<li>禁止将本网站api用于牟利。</li>
+         	<li>欢迎各位捐助。</li>
+         </ol>
     </div>
 </div>
+<br><br>
 <?php require_once "footer.php"; ?>
