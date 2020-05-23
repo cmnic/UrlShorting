a5261e39e0f9bc20f12a0f51ad9891743a034469
立言 <58519324+liyanqwq@users.noreply.github.com>
2020-05-23 13:37:37 +0800

    Update about.php

--- about.php
+++ about.php
@@ -9,25 +9,35 @@ echo <<<EOF
 <br />
 <div class="mdui">
     <!--<div class="mdui-col-sm-6 mdui-col-md-4">-->
-      <div class="mdui-card">
-        <div class="mdui-card-header">
-          <img class="mdui-card-header-avatar" src="./assets/img/logo.png"/>
-          <div class="mdui-card-header-title">星辰短域|密语</div>
-          <div class="mdui-card-header-subtitle">一款简洁美观的短域|密语平台</div>
-        </div>
-        <div class="mdui-card-media">
-          <img src="http://soxft.cn/assets/img/team.png"/>
-        </div>
+      <div class="mdui-card" style="max-width:800px;margin:20px 20px 30px;">
+      <div class="mdui-card-header">
+    <img class="mdui-card-header-avatar" src="https://www.v-gov.net/logo-r.svg"/>
+    <div class="mdui-card-header-title">CMNIC 悄悄话程序</div>
+    <div class="mdui-card-header-subtitle">Powered by <a class="mdui-ripple mdui-text-color-black-text" href="//github.com/soxft/URLshorting">星辰短域|密语</a></div>
+  </div>
   <div class="mdui-card-content">
+<h3>星辰短域|密语 版权信息</h3>
 版本：$version<br/>
 <br/>
 作者：XCSOFT(XSOT.CN)<br/>
 <br/>
 使用语言(框架)：PHP HTML MDUI MYSQL<br/>
+<br/>
+协议：Apache
+<br/><hr/><br/>
+<h3>悄悄话 版权信息</h3>
+版本：1.0<br/>
+<br/>
+作者：Li Yan<br/>
+<br/>
+基于：星辰短域|密语<br/>
+<br/>
+协议：GPLv3
   </div>
   <div class="mdui-card-actions">
-<a class="mdui-btn mdui-ripple" href="//blog.xsot.cn/archives/pro-URLshorting.html">官网</a>
-<a class="mdui-btn mdui-ripple" href="//github.com/soxft/URLshorting">Github</a>
+<a class="mdui-btn mdui-ripple" href="/">官网</a>
+<a class="mdui-btn mdui-ripple" href="https://github.com/cmnic/UrlShorting">Github</a>
+<a class="mdui-btn mdui-ripple" href="//github.com/soxft/URLshorting">星辰短域</a>
   </div>
 </div>
 EOF;
