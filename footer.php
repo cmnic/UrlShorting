b2c2e32b0cade1b23916696ad57febac7345950f
立言 <58519324+liyanqwq@users.noreply.github.com>
2020-05-23 13:59:31 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,26 +1,12 @@
 <!-- 本源码基于apache2开源，你可以在不修改版权的基础上任意修改其他内容 -->
 <br />
 <footer>
-    <script>
-    fetch('https://v1.hitokoto.cn')
-        .then(function (res) {
-        return res.json();
-    })
-        .then(function (data) {
-        var hitokoto = document.getElementById('hitokoto');
-        hitokoto.innerText = data.hitokoto;
-    })
-        .
-    catch (function (err) {
-        console.error(err);
-    })
-    </script>
     <center>
         <div class="mdui-divider"></div>
-        <p id="hitokoto">:D 获取中...</p>
-        <div class="footer-copyright">Powered by <a class="mdui-text-color-grey-800" href="http://xsot.cn">XCSOFT</a>.
-            <br>
-            Fork me on <a class="mdui-text-color-grey-800" href="https://github.com/cmnic/UrlShorting">Github</a>
-        </div>
+        <p id="hitokoto">漫漫长夜总会过去，黎明之日终将到来。</p>
+        <div class="mdui-typo-caption-opacity">&copy; 2020 CMNIC. | Fork me on <a class="mdui-text-color-grey-800" href="https://github.com/cmnic/UrlShorting">Github</a>.<br><br>
+        Copyright © 2019- 2020 XCSOFT All rights reserved.</div>
+        <br/>
+        <iframe src="https://icp.cmnic.org/icp/icon/10000006.html" id="icp-cert-icon" style="border:none;width:250px;height:60px;"></iframe><script src="https://icp.cmnic.org/icp/js/10000006"></script>
     </center>
 </footer>
