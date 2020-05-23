b3f710aa0be06d740c72b7c9bd4b645c837060bd
立言 <58519324+liyanqwq@users.noreply.github.com>
2020-05-23 14:00:01 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,9 +4,8 @@
     <center>
         <div class="mdui-divider"></div>
         <p id="hitokoto">漫漫长夜总会过去，黎明之日终将到来。</p>
-        <div class="mdui-typo-caption-opacity">&copy; 2020 CMNIC. | Fork me on <a class="mdui-text-color-grey-800" href="https://github.com/cmnic/UrlShorting">Github</a>.<br><br>
+        <div class="mdui-typo-caption-opacity">&copy; 2020 CMNIC. | Fork me on <a class="mdui-text-color-grey-800" href="https://github.com/cmnic/UrlShorting">Github</a>.<br/>
         Copyright © 2019- 2020 XCSOFT All rights reserved.</div>
         <br/>
-        <iframe src="https://icp.cmnic.org/icp/icon/10000006.html" id="icp-cert-icon" style="border:none;width:250px;height:60px;"></iframe><script src="https://icp.cmnic.org/icp/js/10000006"></script>
     </center>
 </footer>
